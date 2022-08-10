3eee41a9a075c2f92735357262d5b20c732f9996
justin reid <hi@justinre.id>
2022-08-10 23:53:03 +0100

    mvp reached, 2 orders input

--- models/orders_list.py
+++ models/orders_list.py
@@ -1,6 +1,6 @@
 from models.orders import *
 
 order1 = Order("Duff Beer", 5, "Homer Simpson", "04/12/2021", 6)
-order2 = Order("Buzz Cola", 4, )
+order2 = Order("Buzz Cola", 3, "Milhouse Van Houten", "24/12/2021", 4)
 
-orders = [order1]
+orders = [order1, order2]
