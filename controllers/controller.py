8db0bc0030a322bd0e5aa964419de9310538cde2
justin reid <hi@justinre.id>
2022-08-11 00:38:49 +0100

    extensions complete, moving to css

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,8 @@ from app import app
 from models.orders import Order
 from models.orders_list import *
 
+
+
 # @app.route('/')
 # def index():
 #     return "Hello World!"
@@ -11,3 +13,7 @@ from models.orders_list import *
 def index():
     return render_template('index.html', title='customer orders', orders=orders)
 
+@app.route('/orders/<int:index>', methods=[('GET')])
+def order(index):
+    order = orders[index]
+    return render_template('order.html', title='{{order.product_name}}', order=order)
