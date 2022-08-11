dbbce6aecf7200779fa0729261eb370c59c52cef
justin reid <hi@justinre.id>
2022-08-11 02:24:40 +0100

    i spent way too long on this

--- models/orders_list.py
+++ models/orders_list.py
@@ -2,5 +2,9 @@ from models.orders import *
 
 order1 = Order("Duff Beer", 5, "Homer Simpson", "04/12/2021", 6)
 order2 = Order("Buzz Cola", 3, "Milhouse Van Houten", "24/12/2021", 4)
+order3 = Order("La Bombe", 10, "Homer Simpson", "05/12/2021", 1)
+order4 = Order("Ribwich", 5, "Bart Simpson", "03/12/2021", 1)
+order5 = Order("Guatemalan Insanity Pepper", 1, "Clancy Wiggum", "12/1/2022", 1)
+order6 = Order("Gummi Venus de Milo", 1, "Marge Simpson", "27/11/1994", 1)
 
-orders = [order1, order2]
+orders = [order1, order2, order3, order4, order5, order6]
